16c525ee27f509a7bd7a8a2ef14191b3dc677984
Nteej <140890199+Ntee-j01@users.noreply.github.com>
2024-12-01 02:45:32 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the TermuX-Custom directory
-cd $HOME/TermuX-Custom || exit
+cd $HOME/hacker-style || exit
 
 # Pull the latest changes from the remote repository
 git pull origin main
